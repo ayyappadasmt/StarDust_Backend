fe5f5a1b4442d644f10843d79cb8b3f5af0c44c0
AYYAPPADAS MT <160704510+ayyappadasmt@users.noreply.github.com>
2026-02-14 15:14:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:3000",
-        "https://*.netlify.app"  
+        "https://stardustt.netlify.app"  
     ],
     allow_credentials=True,
     allow_methods=["*"],
