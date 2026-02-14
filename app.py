ff8ff846f9a22ae28c13fb041a511f3fa7203aaa
AYYAPPADAS MT <160704510+ayyappadasmt@users.noreply.github.com>
2026-02-14 14:50:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:3000",
-        "https://your-app-name.netlify.app"  
+        "https://*.netlify.app"  
     ],
     allow_credentials=True,
     allow_methods=["*"],
